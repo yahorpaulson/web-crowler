a218a7bb6f7856cf80da8831c8125e5daad4fd07
yahorpaulson <y.paulson@yahoo.com>
2025-03-16 14:18:52 +0100

    fix: gradle build compatibility, add: fetching method, extracting headings

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Scanner userInput = new Scanner(in);
+
+        //String params = userInput.nextLine();
+
+        int numberOfHeadings = 6;
+
+        //String url = params;
+
+        String url = "https://www.iana.org/help/example-domains";
+
+        try {
+            int depth = 1;
+            getHeadings(url, numberOfHeadings,depth);
+
+        } catch (Exception e){
+            System.out.println("[ERROR]: " + e.getMessage());
+        }
+    }
+
+    public static void getHeadings(String url, int numHeadings, int depth) throws IOException {
+
+        StringBuilder builder = new StringBuilder();
+
+        Document document = Jsoup.connect(url).get();
+
+        for(int i = 1; i<=numHeadings; i++){
+
+            Elements headings = document.select("h" + i);
+
+            builder.append("#" );
+
+            for(Element heading : headings){
+                System.out.println(builder + "-->"+heading.text()+"\n");
+
+            }
+        }
+
+
+    }
+}
