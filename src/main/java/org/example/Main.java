16aacf15c02265b58191d4d27b26c376955b97af
yahorpaulson <y.paulson@yahoo.com>
2025-03-16 14:45:37 +0100

    feature: find links on website and search for headings

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.Scanner;
-
-import static java.lang.System.in;
+import java.util.ArrayList;
 
 
 public class Main {
@@ -22,18 +20,25 @@ public class Main {
 
         //String url = params;
 
-        String url = "https://www.iana.org/help/example-domains";
+        String url = "https://example.com/";
 
         try {
             int depth = 1;
-            getHeadings(url, numberOfHeadings,depth);
+            getHeadings(url, numberOfHeadings);
+
+            ArrayList<String>links = getLinks(url);
+
+            for(String link : links){
+                getHeadings(link, numberOfHeadings);
+            }
+
 
         } catch (Exception e){
             System.out.println("[ERROR]: " + e.getMessage());
         }
     }
 
-    public static void getHeadings(String url, int numHeadings, int depth) throws IOException {
+    public static void getHeadings(String url, int numHeadings) throws IOException {
 
         StringBuilder builder = new StringBuilder();
 
@@ -50,7 +55,18 @@ public class Main {
 
             }
         }
+    }
+
+    public static ArrayList<String> getLinks(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        ArrayList<String> linksList = new ArrayList<>();
+        Elements links = document.select("a[href]");
 
 
+        for(Element link : links){
+            linksList.add(link.attr("abs:href"));
+
+        }
+        return linksList;
     }
 }
