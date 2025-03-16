1cdbfaa932d55a7767fea67b966a67fbeeb59f89
yahorpaulson <y.paulson@yahoo.com>
2025-03-16 18:10:57 +0100

    feature: distribute responsiblilty to methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 
+
 public class Main {
+    private static final int MAX_DEPTH = 2;
+
     public static void main(String[] args) {
 
         //Scanner userInput = new Scanner(in);
@@ -22,15 +26,11 @@ public class Main {
 
         String url = "https://example.com/";
 
+
         try {
-            int depth = 1;
-            getHeadings(url, numberOfHeadings);
+            ArrayList<String> visitedLinks = new ArrayList<>();
 
-            ArrayList<String>links = getLinks(url);
-
-            for(String link : links){
-                getHeadings(link, numberOfHeadings);
-            }
+            startFetch(normalizeUrl(url), 0, numberOfHeadings, visitedLinks);
 
 
         } catch (Exception e){
@@ -38,23 +38,58 @@ public class Main {
         }
     }
 
-    public static void getHeadings(String url, int numHeadings) throws IOException {
+    public static void startFetch(String url, int depth, int numHeadings, ArrayList<String> visited) throws IOException {
+        if(depth >= MAX_DEPTH){
+            return;
+        }
 
-        StringBuilder builder = new StringBuilder();
+        if(visited.contains(normalizeUrl(url))){
+            System.out.println("[VISITED]: " + url);
+            return;
+        }
+        depth++;
+
+        visited.add(normalizeUrl(url));
+        ArrayList<String> links = getLinks(url);
+        System.out.println("input: " + url );
+        System.out.println("<br>depth: " + depth);
+
+        for(String heading : getHeadings(url, numHeadings)){
+            System.out.println(heading);
+        }
+
+        for(String link : links){
+            startFetch(link, depth, numHeadings, visited);
+        }
+    }
+
+
+
+    public static ArrayList<String> getHeadings(String url, int numHeadings) throws IOException {
 
         Document document = Jsoup.connect(url).get();
+
+        ArrayList<String> headingsList = new ArrayList<>();
 
         for(int i = 1; i<=numHeadings; i++){
 
             Elements headings = document.select("h" + i);
 
-            builder.append("#" );
-
             for(Element heading : headings){
-                System.out.println(builder + "-->"+heading.text()+"\n");
-
+                headingsList.add(getHashTag(i) + " ---> "+ heading.text());
             }
         }
+
+        return headingsList;
+    }
+
+    public static String getHashTag(int headingLevel){
+        StringBuilder hashTags = new StringBuilder();
+
+        for(int i = 0; i<headingLevel; i++){
+            hashTags.append("#");
+        }
+        return hashTags.toString();
     }
 
     public static ArrayList<String> getLinks(String url) throws IOException {
@@ -68,5 +103,16 @@ public class Main {
 
         }
         return linksList;
+    }
+
+    private static String normalizeUrl(String url) {
+        if (url.endsWith("/")) {
+            url = url.substring(0, url.length() - 1);
+        }
+        int fragmentIndex = url.indexOf("#");
+        if (fragmentIndex != -1) {
+            url = url.substring(0, fragmentIndex);
+        }
+        return url.toLowerCase();
     }
 }
